35a2e8a1696b73ffff4b0aa06a841443ec3efa9e
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-09-09 18:50:43 +0530

    Add files via upload

---
+++ Q 19 (A).cpp
@@ -0,0 +1,28 @@
+//Write a program to find the LCM of two numbers.
+
+#include <stdio.h>
+
+
+int findHCF(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2, hcf, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    hcf = findHCF(num1, num2);
+    lcm = (num1 * num2) / hcf;
+
+    printf("LCM of %d and %d is: %d\n", num1, num2, lcm);
+
+    return 0;
+}
+
