ee4ea46099a7c36d63501f529f3441bf816b507a
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-09-09 16:07:53 +0530

    Add files via upload

---
+++ Q 12(B).cpp
@@ -0,0 +1,32 @@
+//Write a program to calculate electricity bill based on units consumed with these rates:
+
+#include <stdio.h>
+
+int main() {
+    int units;
+    int bill = 0;
+
+    // Input units consumed
+    printf("Enter total units consumed: ");
+    scanf("%d", &units);
+
+    // Bill Calculation
+    if (units <= 100) {
+        bill = units * 5;
+    }
+    else if (units <= 200) {
+        bill = (100 * 5) + (units - 100) * 7;
+    }
+    else if (units <= 300) {
+        bill = (100 * 5) + (100 * 7) + (units - 200) * 10;
+    }
+    else {
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + (units - 300) * 12;
+    }
+
+    // Display result
+    printf("Total Electricity Bill = ?%d\n", bill);
+
+    return 0;
+}
+
