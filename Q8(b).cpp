4d19c80aa3a84e4b7b6352d155674f8eeec563d3
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-09-09 14:09:55 +0530

    Add files via upload

---
+++ Q8(b).cpp
@@ -0,0 +1,25 @@
+//Write a program to input three numbers and find the largest among them using if–else.
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b) {
+        if (a >= c)
+            printf("%d is the largest number.\n", a);
+        else
+            printf("%d is the largest number.\n", c);
+    }
+    else {
+        if (b >= c)
+            printf("%d is the largest number.\n", b);
+        else
+            printf("%d is the largest number.\n", c);
+    }
+
+    return 0;
+}
+
