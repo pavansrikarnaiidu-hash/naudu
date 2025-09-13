2292c8fbd4ee7f66bd27c95abf308d0d524f3a4f
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-09-13 10:54:09 +0530

    Add files via upload

---
+++ Q24(B).cpp
@@ -0,0 +1,16 @@
+//Write a program to print the following patteN
+#include <stdio.h>
+
+int main() {
+    int i, j, rows = 5;
+
+    for(i = 1; i <= rows; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
