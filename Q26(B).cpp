c53b64478724583a99bc8b07e0c1ff7ab25f1772
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-09-15 09:30:30 +0530

    Add files via upload

---
+++ Q26(B).cpp
@@ -0,0 +1,33 @@
+//Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    // Loop runs 13 times
+    for(i = 0; i < 13; i++) {
+        printf("*\n");
+    }
+    return 0;
+}
+
