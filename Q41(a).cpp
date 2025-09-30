878015d3ad08e692a77e1bd7eb9b6c7cc5b2d929
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-09-30 14:17:13 +0530

    Add files via upload

---
+++ Q41(a).cpp
@@ -0,0 +1,18 @@
+//Count characters in a string without using built-in length functions
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0;
+
+    printf("Enter a string:\n");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        length++;
+    }
+
+    printf("Length of input string: %d\n", length);
+    return 0;
+}
+
