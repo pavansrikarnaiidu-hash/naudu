cd4b4ad4c2a007f1fb9d9286b353a2a5dd3578ca
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-10-18 09:27:35 +0530

    Add files via upload

---
+++ Q59.cpp
@@ -0,0 +1,38 @@
+//Write a program to take an integer array arr and an integer k as inputs. Print the maximum sum of all the subarrays of size k
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the size of subarray (k): ");
+    scanf("%d", &k);
+
+    if (k > n || k <= 0) {
+        printf("Invalid subarray size.\n");
+        return 0;
+    }
+
+    int current_sum = 0;
+    for (int i = 0; i < k; i++)
+        current_sum += arr[i];
+
+    int max_sum = current_sum;
+
+    for (int i = k; i < n; i++) {
+        current_sum += arr[i] - arr[i - k];
+        if (current_sum > max_sum)
+            max_sum = current_sum;
+    }
+
+    printf("Maximum sum of subarray of size %d is: %d\n", k, max_sum);
+    return 0;
+}
+
