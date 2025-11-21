f447a1f79e3cec56873c598219605bc6cc357529
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-11-21 08:40:09 +0530

    Add files via upload

---
+++ Q93.cpp
@@ -0,0 +1,36 @@
+//Find and print the student with the highest marks.
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    char name[50];
+    int marks;
+};
+
+int main() {
+    int n;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    struct Student students[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter name of student %d: ", i + 1);
+        scanf("%s", students[i].name);
+        printf("Enter marks of student %d: ", i + 1);
+        scanf("%d", &students[i].marks);
+    }
+
+    int maxIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (students[i].marks > students[maxIndex].marks) {
+            maxIndex = i;
+        }
+    }
+
+    printf("\nStudent with highest marks:\n");
+    printf("%s %d\n", students[maxIndex].name, students[maxIndex].marks);
+
+    return 0;
+}
+
